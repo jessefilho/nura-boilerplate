8c1132632392c51e0f22c56dbd9beecc6bd8b9a0
Jesse <jessefilhojfnf@gmail.com>
2023-01-06 18:26:48 +0100

    update vuetify plugin

--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,11 +1,45 @@
+// plugins/vuetify.js
+
+import 'assets/scss/variable.scss'
+import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+// import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import { aliases,mdi } from 'vuetify/iconsets/mdi'
+const theme_light = {
+    dark: false,
+    colors: {
+        primary: '#008DDF',
+        secondary: '#424242',
+        accent: '#9550C7',
+        error: '#af0020',
+        info: '#0070b8',
+        success: '#429E45',
+        warning: '#E07B00',
+    }
+}
+
 
 export default defineNuxtPlugin(nuxtApp => {
+
     const vuetify = createVuetify({
+        treeShake: true, //  required for custom SASS variables to work
         components,
         directives,
+        icons: {
+            defaultSet: 'mdi', //default
+            aliases,
+            sets: {
+                mdi,
+            },
+        },
+        theme: {
+            defaultTheme: 'theme_light',
+            themes: {
+                theme_light,
+            }
+        }
     })
 
     nuxtApp.vueApp.use(vuetify)
